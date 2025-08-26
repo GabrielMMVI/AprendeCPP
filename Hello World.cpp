376659c0298c204a5532f8efcb880f3bc652ab6f
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-08-26 17:16:28 -0300

    HelloWorld

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,1 +1,20 @@
-printl("Hello world!")
+#include <iostream>
+using namespace std; //-> Uma biblioteca que organiza pasta, 
+// Biblioteca iostream -> biblioteca de entrada e saida de dados 
+/* Comentário é assim*/
+
+
+//Para começar o código tem que declarar o main antes
+
+int main(){
+
+cout << "Hello World!"; //cout -> Console out
+
+return 0;
+}
+
+//Uma função que retorna um inteiro (0)
+
+
+
+
