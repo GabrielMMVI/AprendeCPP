66d488a2ffe166967aa75c7817f78c0b240e671d
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-08-27 15:57:16 -0300

    Hello world novo

--- Hello World.cpp
+++ Hello World.cpp
@@ -2,13 +2,17 @@
 using namespace std; //-> Uma biblioteca que organiza pasta, 
 // Biblioteca iostream -> biblioteca de entrada e saida de dados 
 /* Comentário é assim*/
+#include<locale.h> //Biblioteca de acentos ´ ^ ~
 
 
 //Para começar o código tem que declarar o main antes
 
 int main(){
+	
+	setlocale(LC_ALL, ""); //
 
-cout << "Hello World!"; //cout -> Console out
+cout << "Hello World!" << endl; //cout -> Console out  //endl tambem quebra linha
+cout << "Olá! Mundo!\n bananations"; //quebra de linha é só n minúsculo
 
 return 0;
 }
