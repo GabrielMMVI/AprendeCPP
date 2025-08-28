799a32f4bdbd2c4bb797ec5d4af8ccf68ce00002
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-08-28 10:02:11 -0300

    Variaveis criada

--- Variavies.cpp
+++ Variavies.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-#include <localhe.h>
+#include <locale.h>
 using namespace std;
 
 int main (){
 	
-	int numInt = 
+	int numInt = 12;
+	double numDec = 2.14 ;
+	char letra = 'a'; //Aceita somente um caracter
+	const char* letras = "abcd"; //Aceita vários
+	string txt = "Texto completo"; //String básica
+	bool VF = false; // false = 0 ; true = 1
+	
+	cout << numInt << endl;
+	cout << numDec << endl;
+	cout << letra << endl;
+	cout << letras << endl;
+	cout << txt << endl;
+	cout << VF << endl;
+	
 	
 }
 
