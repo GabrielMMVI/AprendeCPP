9f793870f4875bd0d1691c5264c75fd6d30ecb47
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-08-29 10:28:57 -0300

    VarGlobal

--- VariaveisGlobais.cpp
+++ VariaveisGlobais.cpp
@@ -3,3 +3,38 @@
 using namespace std;
 
 
+// Variáveis globais são mutáveis e definidas por todo o código
+
+double nota1, nota2, nota3, nota4; //->Globais
+int main () {
+	setlocale(LC_ALL, "Portuguese");
+	
+	double media; //Variável local, só é acessada por essa função
+	
+	cout << "Calcula nota do aluno\n";
+	cout << "==========================\n";
+	
+	cout << "Insira a 1a nota do aluno: ";
+	cin >> nota1;
+	
+	cout << "Insira a 2a nota do aluno: ";
+	cin >> nota2;
+	
+	cout << "Insira a 3a nota do aluno: ";
+	cin >> nota3;
+	
+	cout << "Insira a 4a nota do aluno: ";
+	cin >> nota4;
+	
+	cout << "Calculando média...\n";
+	system("@echo off");
+	system("Pause");
+	
+	media = (nota1 + nota2 + nota3 + nota4)/4;
+	
+	cout << "A média desse aluno foi de: " << media <<"\n";
+	
+	system("pause");
+	return 0;
+
+}
