0e2122ab529f1268697ce7636d8d62054eb3ce34
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-08-29 17:02:56 -0300

    Incremento de variáveis

--- IncrementoVar.cpp
+++ IncrementoVar.cpp
@@ -5,8 +5,18 @@ using namespace std;
 int main(){
 	setlocale(LC_ALL,"portuguese");
 	
+	int num;
 	
+	num = 0;
+	cout << "Valor inicial da variável: ";
+	cout << num << "\n";
+	//Aqui ela só foi iniciada como 0
+	cout << "=-=-=-=-=-=-=-=-=-=-=\n\n";
 	
+	//Soma +1 na variável
+	num = num+1;
+	
+		
 	return 0;
 	system("pause");
 }
