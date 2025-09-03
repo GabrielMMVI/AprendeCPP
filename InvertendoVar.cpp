7e0c0f62a7eea335a4d8138c43c401dbcb011370
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-03 09:13:05 -0300

    Invertendo variáveis

--- InvertendoVar.cpp
+++ InvertendoVar.cpp
@@ -5,6 +5,36 @@ using namespace std;
 int main(){
 	setlocale(LC_ALL, "portuguese");
 	
+	int num;
+	
+	cout << "Iniciando a var 'num'\n";
+	num = 10;
+	cout << num;
+	
+	cout << "\n\n";
+	
+	//Invertendo a variável
+	
+	num = num*-1;
+	cout << "Valor inverso da variável: \n";
+	cout << num;
+	
+	cout << "\n\n";
+	
+	cout << "-=-=-=-PRÉ FIXADO-=-=-=-\n\n";
+	num = 20;
+	cout << "Valor inicial da variável: \n";
+	cout << num << "\n";
+	cout << "Valor inverso da variável usando prefixação (-num): \n";
+	cout << -num << "\n\n";
+	
+	cout << "-=-=-=-SUBSTITUIDO-=-=-=-\n\n";
+	num = 30;
+	cout << "Valor inicial da variável: \n";
+	cout << num << "\n";
+	cout << "Valor inverso da variável usando substituição (num=-num): \n";
+	num=-num;
+	cout << num << "\n";
 	
 	
 	return 0;
