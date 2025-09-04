c40a38f65696a818541d1196cde7716f95eeac66
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-04 16:55:12 -0300

    Operador%

--- Operador%.cpp
+++ Operador%.cpp
@@ -5,8 +5,14 @@ using namespace std;
 int main(){
 	setlocale(LC_ALL, "portuguese");
 	//Operador %
+	int num, resto;
 	
+	num = 10;
+	resto = num % 3;
+	cout << "% serve para emitir o resto de uma divisão: \n\n";
 	
+	cout << "fiz 10%3, logo o resto será 1: \n";
+	cout << resto << "\n";
 	
 
 	system("Pause");
