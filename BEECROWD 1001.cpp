2a0cb27c9f1f1b2766b82896f1c900e00d5bebe2
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-22 16:48:32 -0300

    Create BEECROWD 1001.cpp

---
+++ BEECROWD 1001.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+ 
+int main() {
+ 
+    int A, B, X;
+    cin >> A;
+    cin >> B;
+    
+    X = A+B;
+    
+    cout << X << endl;
+    
+ 
+    return 0;
+}
