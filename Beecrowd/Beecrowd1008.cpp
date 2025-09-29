099aee4f5615b472629d258f0134e384f1923eb0
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-29 20:43:13 -0300

    Beecrowd 7 e 8 Feitos

---
+++ Beecrowd/Beecrowd1008.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+using namespace std;
+
+int main () {
+	
+	int n, hr;
+	double vlhr ,sal;
+	
+	cin >> n;
+	cin >> hr;
+	cin >> vlhr;
+	
+	sal = hr * vlhr;
+	
+	cout << "NUMBER = " << n << endl;
+	cout << "SALARY = " << fixed << setprecision(2) << sal << endl;
+	
+}
