d5c3170c921f62abd2e01613a5bf310b0da0374e
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-10-07 11:12:33 -0300

    Beecrowd 1010

--- Beecrowd/Beecrowd1010.cpp
+++ Beecrowd/Beecrowd1010.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <limits>
+#include <iomanip>
+using namespace std;
 
 int main (){
+	int qt1, qt2, id1, id2;
+	double preco1, preco2, precoTotal;
 	
+	cin >> id1;
+	cin >> qt1;
+	cin >> preco1; 
+	
+	cin >> id2;
+	cin >> qt2;
+	cin >> preco2;
+	
+	precoTotal = qt1 * preco1 + qt2 * preco2;
+	
+	cout << fixed << setprecision(2) << "VALOR A PAGAR: R$ " << precoTotal << endl;
+	
+	return 0;
 	
 	
 }
