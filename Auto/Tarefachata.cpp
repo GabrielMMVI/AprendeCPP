f255858fa34462b2a7495b95b0e735a0cb223954
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-10-10 17:14:38 -0300

    Add new exercises and update Tarefachata logic
    Added Beecrowd1013 and ExerVerificaGen source and executable files. Updated Auto/Tarefachata.cpp to iterate over multiple versions and numbers, improving SQL input generation logic.
    
    TarefaChata is not finished yet,
    need to fix the repetition cycle to set the right values to each version,
    Need to create parameters to set customizable path for the executable.

--- Auto/Tarefachata.cpp
+++ Auto/Tarefachata.cpp
@@ -2,14 +2,28 @@
 using namespace std;
 
 int main (){
-	int num, i;
+	int num, v;
 	string texto;
 	num = 1;
-	do {
-		cout << "INPUT 'C:/BdAtt/AtualizaBancoV10."<< num <<".sql';" << endl;
-		num++;
-        
-    } while (num != 99); 
-    return 0;
+	v = 1;
+loop:
+	
+	if(num < 100)
+	{
+	cout << "INPUT 'C:/BdAtt/AtualizaBancoV" << v << "." << num <<".sql';" << endl; 
+	num++;
+	} 
+	
+	else if(num == 100 && v < 13) 
+	{
+	v++;
+	num = 1;	
+	} 
+	
+	else{return 0;}
+	
+
+	return 0;
+	
 	
 }
---
+++ ExerVerificaGen.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main (){
+	setlocale(LC_ALL, "portuguese");
+	char gen;
+	
+	cin >> gen;
+/*	
+	(gen == 'F' || gen == 'f')?
+	cout << "Gênero feminino\n";
+	:
+	(gen == 'M' || gen == 'm')?
+	cout << "Gênero masculino\n";
+	:
+	(gen != 'f' && gen != 'F' && gen !='m' && gen !='M')?
+	cout << "Gênero inválido\n";
+*/	
+
+	if(gen == 'F' || gen == 'f')
+	{
+		cout << "Gênero feminino\n";	
+	}
+	else if(gen == 'M' || gen == 'm')
+	{
+		cout << "Gênero masculino\n";
+	}
+	else
+	{
+		cout << "Gênero não definido.\n";
+	}
+	
+	
+	
+}
