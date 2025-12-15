4d67ce32d2fe77ebc16fc85d8e882530df5e5628
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-12-15 17:41:13 -0300

    Update ExercSwitch.cpp

--- ExercSwitch.cpp
+++ ExercSwitch.cpp
@@ -8,8 +8,12 @@ int main(){
 	int n1, n2;
 	char c;
 	
-	c = '+';
-	 cout << c;
+	cout << "=+=+= FAZEDOR DE OPERAÇÕES =+=+=\n";
+	cout << "==================================\n";
+	cout << "Selecione uma operação para fazer: \n\n";
+	cout << "+ | Soma\n" << "- | Subtração\n" << "* | Multiplicação\n" << "/ | Divisão\n";
+	cout << endl << endl;
+	
 	
 	
 }
