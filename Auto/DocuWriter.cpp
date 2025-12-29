457df16ae13c7af4df05a8f9dc882d34a287c624
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-12-29 08:52:35 -0300

    Automação eMail
    Não está pronto

---
+++ Auto/DocuWriter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main(){
+	int opc;
+	string att;
+	cout << " -- ESCREVA TUDO EM LETRA MAIUSCULA!! -- " << endl;
+	
+	cout << "O que foi atualizado?\n";
+	cout << "BANCO -> Banco de dados || DOC -> Projeto (Release)\n";
+	cout << ">_";
+	cin >> att;
+	cout << att << endl;
+
+	
+}
