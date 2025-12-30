8b04f15b5bf94cf7a3414f34360590ed8d454821
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-12-30 17:10:11 -0300

    While n terminei

---
+++ ExercWhile4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <locale.h>
+#include <limits>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
+	int num, i, o, p, q;
+	
+	while (num >=0){	
+	cout << "Digite um número: ";
+	cin >> num;
+	i++;
+	
+	cout << "i: "<< i << endl;
+	cout << "num: "<< num << endl;
+	
+	if(i==10){
+		break;
+		
+		while(num >=0 && i=10){
+			
+		}
+	}
+}
+	
+	
+	system("pause");
+	return 0;
+
+}
