af93ea1cc4f9f6ed338723a4c5b86c9811fb85fd
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2026-01-08 15:07:44 -0300

    Finalizado exercício While

--- ExercWhile4.cpp
+++ ExercWhile4.cpp
@@ -6,28 +6,38 @@ using namespace std;
 
 int main(){
 	setlocale(LC_ALL,"Portuguese");
-	
-	int num, i, o, p, q;
-	
-	while (num >=0){	
-	cout << "Digite um número: ";
-	cin >> num;
-	i++;
-	
-	cout << "i: "<< i << endl;
-	cout << "num: "<< num << endl;
-	
-	if(i==10){
-		break;
+//Variáveis
+	int numInt = 0; //Número recebido
+	int i = 0; int o= 0; int p = 0; int q = 0; int z = 0; //Operadores
+
+	while(true){
+		cout << z << " | numInt: ";
+		cin >> numInt;
+		z++;
 		
-		while(num >=0 && i=10){
+		if(numInt > 40){
+			cout << "numInt > 40!\nAbortando operação!\n\n";
+			break;
+			system("pause");
+			return 0;
+			}	
+		
+		else if (0  <= numInt && numInt <= 10){i++;}
+		else if (11 <= numInt && numInt <= 20){o++;}
+		else if (21 <= numInt && numInt <= 30){p++;}
+		else if (31 <= numInt && numInt <= 40){q++;}
+		
+		else if (numInt < 0){
 			
+			cout << "i: " << i << endl;
+			cout << "o: " << o << endl;
+			cout << "p: " << p << endl;
+			cout << "q: " << q << endl;
+			
+			break;
+			system("pause");
+			return 0;
 		}
 	}
-}
+}	
 	
-	
-	system("pause");
-	return 0;
-
-}
